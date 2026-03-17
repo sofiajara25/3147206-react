a288833c7313f1a4dee82fef10d96d6d28da2227
sofiajara25 <sofiajaramilloor16@gmail.com>
2026-03-17 15:06:55 -0500

    proyecto-formativo

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App () {
+  return(
+    <div className="min-h-screen bg-green-800 flex items-center justify-center">
+      <h1 className="text-white text-4xl font-bold">
+        Con Sofia Jaramillo Tailwind v4 funciona full...
+      </h1>
+    </div>
+  );
+}
